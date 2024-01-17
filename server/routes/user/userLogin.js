fb6c73ab82ce37b712e01162892789ff6e1279bb
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2024-01-17 17:35:41 +0530

    changes

--- server/routes/user/userLogin.js
+++ server/routes/user/userLogin.js
@@ -263,28 +263,38 @@ router.post('/verifyfirebaselogintoken', async (req, res) => {
     try {
       const decodedToken = await admin.auth().verifyIdToken(idToken);
       const uid = decodedToken.uid;
-  
+      let user;
       // Find or create the user in your database
       const userObj = {
-        uid,
-        email: decodedToken.email,
-        full_name: decodedToken.name,
-        joining_date:new Date(),
-        role: new ObjectId('659fdac630fa1324fb3d2688'),
-        creation_process:'Auto SignUp',
-        status:'Active'
-        // ... any other user fields
-      };
-      if(decodedToken?.picture){
-        userObj.profile_picture.url = decodedToken?.picture;
-      }
-      if(decodedToken?.phone_number){
-        userObj.mobile = decodedToken.phone_number.replace(/^\+91/, '');
-      }
-      const user = await UserDetail.findOneAndUpdate({ uid }, userObj, { new: true, upsert: true });
+          uid,
+          email: decodedToken.email,
+          full_name: decodedToken.name,
+          joining_date:new Date(),
+          role: new ObjectId('659fdac630fa1324fb3d2688'),
+          creation_process:'Auto SignUp',
+          status:'Active'
+          // ... any other user fields
+        }
+        if(decodedToken?.picture){
+            userObj.profile_picture = {url:decodedToken?.picture, name:decodedToken?.picture};
+        }
+        if(decodedToken?.phone_number){
+            userObj.mobile = decodedToken.phone_number.replace(/^\+91/, '');
+        }
+        if(await UserDetail.findOne({email:decodedToken?.email})){
+            user = await UserDetail.findOneAndUpdate({email: decodedToken?.email }, userObj, { new: true, upsert: true });
+            const token = jwt.sign({ _id:user?._id }, process.env.SECRET_KEY);
+            return res.status(200).json({ status: 'success', message: "User login successful", token: token });
+        }  
+        if(await UserDetail.findOne({mobile:decodedToken?.phone_number})){
+            user = await UserDetail.findOneAndUpdate({email: decodedToken?.email }, userObj, { new: true, upsert: true });
+            const token = jwt.sign({ _id:user?._id }, process.env.SECRET_KEY);
+            return res.status(200).json({ status: 'success', message: "User login successful", token: token });
+        }  
+         user = await UserDetail.findOneAndUpdate({ uid }, userObj, { new: true, upsert: true });
   
       // Create a JWT token
-      const token = jwt.sign({ _id }, process.env.SECRET_KEY);
+      const token = jwt.sign({ _id:user?._id }, process.env.SECRET_KEY);
       return res.status(200).json({ status: 'success', message: "User login successful", token: token });
     } catch (error) {
         console.log(error);
