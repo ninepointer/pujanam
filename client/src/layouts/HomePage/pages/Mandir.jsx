d1780682a396c9a241e00f8b09f44671d9f82741
Prateek Pawan <prats24@gmail.com>
2024-01-17 22:11:26 +0530

    hi

--- client/src/layouts/HomePage/pages/Mandir.jsx
+++ client/src/layouts/HomePage/pages/Mandir.jsx
@@ -1,46 +1,79 @@
-import { Container, Grid, Stack } from '@mui/material'
+import { Grid } from '@mui/material'
 import { ThemeProvider } from 'styled-components';
-import React, {useState, useContext, useEffect} from "react"
+import React, {useState, useEffect} from "react"
 import ReactGA from "react-ga";
 import axios from "axios";
-import Title from '../components/Title/index'
-import ServiceCard from '../components/Cards/ServiceCard'
 import PageNavbar from '../components/Navbars/PageNavbar'
 import theme from '../utils/theme/index'
 import background from '../../../assets/images/background.jpg'
-import logo from '../../../assets/images/logo.png'
+import dhamicon from '../../../assets/images/dham.png'
+import nearmeicon from '../../../assets/images/nearme.png'
+import popularnearmeicon from '../../../assets/images/popular.png'
 import Footer from '../../authentication/components/Footer'
 import { apiUrl } from '../../../constants/constants';
 import MDBox from '../../../components/MDBox';
 import MDTypography from '../../../components/MDTypography';
-import {Typography, CardActionArea, CardContent, Card, Divider, CircularProgress} from '@mui/material';
+import {Typography} from '@mui/material';
 import MandirCard from './MandirCard'
-import { PowerInput } from '@mui/icons-material';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import TabPanel from '@mui/lab/TabPanel';
+import MDAvatar from '../../../components/MDAvatar';
 
+function TabPanel1(props) {
+  const { children, value, index, ...other } = props;
 
-const LinkButton = ({ children, ...props }) => (
-    <Stack
-      direction="row"
-      alignItems="center"
-      spacing={0.2}
-      sx={{
-        cursor: "pointer",
-        color: "#315c45",
-        "&:hover": { color: '#65BA0D'},
-      }}
-      {...props}
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
     >
-      {children}
-    </Stack>
+      {value === index && (
+        <Box sx={{ p: 0 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
   );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
 
 const About = () => {
-    console.log(theme);
+    const [value, setValue] = React.useState(0);
     const [scrollPosition, setScrollPosition] = useState(0);
     const [data,setData] = useState([]);
     const [dham,setDham] = useState([]);
     const [popular,setPopular] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+  
+    const handleChangeIndex = (index) => {
+      setValue(index);
+    };
+  
 
     useEffect(()=>{
         let call1 = axios.get(`${apiUrl}usermandir/allhome`,{
@@ -75,7 +108,7 @@ const About = () => {
           setPopular(api1Response3?.data?.data)
           setTimeout(()=>{
             setIsLoading(false)
-          },200)
+          },500)
         })
         .catch((error) => {
           // Handle errors here
@@ -106,8 +139,7 @@ const About = () => {
     
     const backgroundColor = scrollPosition > 10 ? 'rgba(0, 0, 0, 0.8)' : 'transparent'
     const backdropFilter = scrollPosition > 10 ? 'blur(5px)' : 'none'
-    
-    console.log("Dham1:",dham)
+
     return (
         <>
       <MDBox mt={-1} display='flex' justifyContent='center' flexDirection='column' alignContent='center' alignItems='center' style={{ minHeight:'auto', width: 'auto', minWidth:'100vW', overflow: 'visible'}}>
@@ -135,8 +167,201 @@ const About = () => {
           >
 
           </Grid>
+
+          <Grid container mt={15} mb={2} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' xs={12} md={12} lg={12} style={{ maxWidth: '95%', height: 'auto', flexGrow: 1, overflowY: 'auto', zIndex:3, overflow: 'visible' }}>
+          <AppBar position="static" style={{borderBottom:'0.5px solid white',backgroundColor:"rgba(0, 0, 0, 0)", color:"red", display:'flex', maxWidth: '100%'}}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="secondary"
+              textColor="inherit"
+              variant="fullWidth"
+              aria-label="full width tabs example"
+              style={{backgroundColor:"rgba(0, 0, 0, 0)", color:"red", maxWidth:'60%'}}
+            >
+              <Tab 
+                // label="About 4 Dham" 
+                disableRipple="true" {...a11yProps(0)} 
+                label={
+                  <Grid container xs={12} md={12} lg={12} display='flex' justifyContent='center' alignItems='center' alignContent='center'>
+                    <Grid xs={12} md={12} lg={12} item display='flex' justifyContent='center' alignItems='center' alignContent='center'>
+                      <MDAvatar size='md' src={dhamicon} color='success'/>
+                    </Grid>
+                    <Grid xs={12} md={12} lg={12} item display='flex' justifyContent='center' alignItems='center' alignContent='center'>
+                      About 4 Dham
+                    </Grid>   
+                  </Grid>
+                }
+              />
+              <Tab 
+                // label="Popular Mandir near me" 
+                disableRipple="true" 
+                {...a11yProps(1)}
+                label={
+                  <Grid container xs={12} md={12} lg={12} display='flex' justifyContent='center' alignItems='center' alignContent='center'>
+                    <Grid xs={12} md={12} lg={12} item display='flex' justifyContent='center' alignItems='center' alignContent='center'>
+                      <MDAvatar size='md' src={popularnearmeicon} color='success'/>
+                    </Grid>
+                    <Grid xs={12} md={12} lg={12} item display='flex' justifyContent='center' alignItems='center' alignContent='center'>
+                      Popular Mandir near me
+                    </Grid>   
+                  </Grid>
+                }
+                />
+              <Tab 
+                // label="Mandir near me" 
+                disableRipple="true" 
+                {...a11yProps(2)} 
+                label={
+                  <Grid container xs={12} md={12} lg={12} display='flex' justifyContent='center' alignItems='center' alignContent='center'>
+                    <Grid xs={12} md={12} lg={12} item display='flex' justifyContent='center' alignItems='center' alignContent='center'>
+                      <MDAvatar size='md' src={nearmeicon} color='success'/>
+                    </Grid>
+                    <Grid xs={12} md={12} lg={12} item display='flex' justifyContent='center' alignItems='center' alignContent='center'>
+                      Mandir near me
+                    </Grid>   
+                  </Grid>
+                }
+                />
+            </Tabs>
+          </AppBar>
+          <SwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            {!isLoading ? 
+            <TabPanel1 value={value} index={0} dir={theme.direction}>
+                <Grid item mt={2} mb={1} xs={12} md={12} lg={12} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ width: '100%', height: 'auto' }}>
+                <Grid container xs={12} md={12} lg={12} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                    <Grid ml={2} item xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                      <MDBox display='flex' justifyContent='flex-start' alignContent='flext-start' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                        <MDTypography variant="h4" color="dark">All about 4 dham mandir!</MDTypography>
+                      </MDBox>
+                    </Grid>
+
+                    <Grid mt={1} ml={2} item xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{backgroundColor:'white', borderRadius:10 ,maxWidth: '100%', height: 'auto' }}>
+                      <MDBox p={2} display='flex' justifyContent='center' alignContent='flext-start' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                        <MDTypography variant="body2" color="dark">Char Dham are the four places in India with high pilgrimage importance. These four places are namely Rameshwaram, Jaganath-Puri, Badrinath-Kedarnath and Dwarka. Adi Shankaracharya defined the term Char Dham as the three Vaishnavite, than one Shaivite and the last a state of mixed ones. It is some of the most holy places that a Hindu should visit, at least once in a lifetime. The origin of these four temples here is still not well known and has remained to be a mysterious fact. Shankaracharya is said to have established the Advaita school of Hinduism, which was also accountable for the origin of the Char Dham. The four Hindu temples are lying across the four corners of India. The Badrinath temple is in the north, followed by the east temple of Jaganath Puri, the Dwarkadhish Temple located at Dwarka in the west, and Rameswaram in the south, the Ramanathaswamy Temple.</MDTypography>
+                      </MDBox>
+                    </Grid>
+
+                    {dham?.length > 0 ?
+                        <Grid item xs={12} mt={2} md={12} lg={12} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+
+                        <MDBox display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                            <Grid container spacing={4} xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{ width: '100%', height: 'auto' }}>
+                            {dham?.map((elem) => {
+                                return (
+                                <Grid key={elem?._id} item xs={12} md={4} lg={3} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ width: '100%', height: 'auto' }}>
+                                    <MandirCard elem={elem}/>
+                                </Grid>
+                                )
+                            })}
+                            </Grid>
+                        </MDBox>
+
+                        </Grid>
+                        :
+                        <>
+                        {/* <img src={NoData} width='500px' height='500px' /> */}
+                        </>
+                    }
+
+                </Grid>
+                </Grid>
+            </TabPanel1>
+            :
+            <TabPanel1 value={value} index={0} dir={theme.direction}>
+            <MDBox display='flex' justifyContent='center' alignContent='center' alignItems='center'>
+              <MDTypography>Hi</MDTypography>
+            </MDBox>
+            </TabPanel1>
+            }
+            <TabPanel1 value={value} index={1} dir={theme.direction}>
+                <Grid item mt={2} mb={1} xs={12} md={12} lg={12} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ width: '100%' }}>
+                <Grid container xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                    <Grid ml={2} item xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                      <MDBox display='flex' justifyContent='center' alignContent='flext-start' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                        <MDTypography variant="h4" color="dark">Popular Mandir near you!</MDTypography>
+                      </MDBox>
+                    </Grid>
+
+                    <Grid ml={2} item xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                      <MDBox display='flex' justifyContent='center' alignContent='flext-start' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                        <MDTypography variant="body2" color="dark">All Mandir near you!</MDTypography>
+                      </MDBox>
+                    </Grid>
+
+                    {popular?.length > 0 ?
+                        <Grid item xs={12} mt={2} md={12} lg={12} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+
+                        <MDBox display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                            <Grid container spacing={4} xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{ width: '100%', height: 'auto' }}>
+                            {popular?.map((elem) => {
+                                return (
+                                <Grid key={elem?._id} item xs={12} md={4} lg={3} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ width: '100%', height: 'auto' }}>
+                                    <MandirCard elem={elem}/>
+                                </Grid>
+                                )
+                            })}
+                            </Grid>
+                        </MDBox>
+
+                        </Grid>
+                        :
+                        <>
+                        {/* <img src={NoData} width='500px' height='500px' /> */}
+                        </>
+                    }
+
+                </Grid>
+                </Grid>
+            </TabPanel1>
+            <TabPanel1 value={value} index={2} dir={theme.direction}>
+                <Grid item mt={2} mb={1} xs={12} md={12} lg={12} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ width: '100%' }}>
+                <Grid container xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                    <Grid ml={2} item xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                      <MDBox display='flex' justifyContent='center' alignContent='flext-start' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                        <MDTypography variant="h4" color="dark">All Mandir near you!</MDTypography>
+                      </MDBox>
+                    </Grid>
+
+                    <Grid ml={2} item xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                      <MDBox display='flex' justifyContent='center' alignContent='flext-start' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                        <MDTypography variant="body2" color="dark">All Mandir near you!</MDTypography>
+                      </MDBox>
+                    </Grid>
+
+                    {data?.length > 0 ?
+                        <Grid item xs={12} mt={2} md={12} lg={12} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+
+                        <MDBox display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ maxWidth: '100%', height: 'auto' }}>
+                            <Grid container spacing={4} xs={12} md={12} lg={12} display='flex' justifyContent='flex-start' alignContent='center' alignItems='center' style={{ width: '100%', height: 'auto' }}>
+                            {data?.map((elem) => {
+                                return (
+                                <Grid key={elem?._id} item xs={12} md={4} lg={3} display='flex' justifyContent='center' alignContent='center' alignItems='center' style={{ width: '100%', height: 'auto' }}>
+                                    <MandirCard elem={elem}/>
+                                </Grid>
+                                )
+                            })}
+                            </Grid>
+                        </MDBox>
+
+                        </Grid>
+                        :
+                        <>
+                        {/* <img src={NoData} width='500px' height='500px' /> */}
+                        </>
+                    }
+
+                </Grid>
+                </Grid>
+            </TabPanel1>
+          </SwipeableViews>
+              </Grid>
           
-          {!isLoading ?
+          {/* {!isLoading ?
             <>
             <Grid container mt={12} mb={2} display='flex' justifyContent='center' alignContent='center' alignItems='center' xs={12} md={12} lg={12} style={{ maxWidth: '100%', height: 'auto', flexGrow: 1, overflowY: 'auto', zIndex:3, overflow: 'visible' }}>
 
@@ -169,7 +394,7 @@ const About = () => {
                                 </Grid>
                                 :
                                 <>
-                                {/* <img src={NoData} width='500px' height='500px' /> */}
+                                
                                 </>
                             }
 
@@ -202,7 +427,7 @@ const About = () => {
                                 </Grid>
                                 :
                                 <>
-                                {/* <img src={NoData} width='500px' height='500px' /> */}
+                                
                                 </>
                             }
 
@@ -235,7 +460,7 @@ const About = () => {
                                 </Grid>
                                 :
                                 <>
-                                {/* <img src={NoData} width='500px' height='500px' /> */}
+                                
                                 </>
                             }
 
@@ -250,7 +475,7 @@ const About = () => {
             <MDBox mt={35} mb={35} display="flex" width="100%" justifyContent="center" alignItems="center">
                 <CircularProgress color='success' />
             </MDBox>
-         }
+         } */}
         </ThemeProvider>
       </MDBox>
 
